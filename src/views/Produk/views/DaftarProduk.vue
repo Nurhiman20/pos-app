cd9a51c40cc124f2a340001091bb1b7389991e06
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2021-01-09 12:48:47 +0700

    feat(Product): Edit product

--- src/views/Produk/views/DaftarProduk.vue
+++ src/views/Produk/views/DaftarProduk.vue
@@ -97,6 +97,67 @@
         </ValidationObserver>
       </v-card>
     </v-dialog>
+
+    <!-- dialog add product -->
+    <v-dialog v-model="dialogEditProduct" persistent width="400">
+      <v-card class="pa-3">
+        <v-card-title class="ml-0">Edit Produk</v-card-title>
+        <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+          <v-form @submit.prevent="handleSubmit(editProduct)">
+            <ValidationProvider v-slot="{ errors }" name="Nama produk" rules="required">
+              <v-text-field
+                :error-messages="errors"
+                v-model="selectedProduct.name"
+                label="Nama Produk"
+                outlined
+                dense
+                class="mb-0 mt-2 px-4"
+              ></v-text-field>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors }" name="Harga produk" rules="required|integer">
+              <v-text-field
+                :error-messages="errors"
+                v-model="selectedProduct.price"
+                label="Harga Produk"
+                placeholder="15000"
+                outlined
+                dense
+                class="mb-0 mt-2 px-4"
+              ></v-text-field>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors }" name="Kategori produk" rules="required">
+              <v-select
+                v-model="selectedProduct.category"
+                :error-messages="errors"
+                :items="listCategory"
+                :item-text="textCategory"
+                :item-value="valueCategory"
+                label="Kategori Produk"
+                class="mb-0 mt-2 px-4"
+                outlined
+              ></v-select>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors }" name="Deskripsi produk" rules="required|max:200">
+              <v-textarea
+                v-model="selectedProduct.description"
+                :error-messages="errors"
+                label="Deskripsi Produk"
+                class="mb-0 mt-2 px-4"
+                rows="3"
+                outlined
+                counter
+                auto-grow
+              ></v-textarea>
+            </ValidationProvider>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="error darken-1" text @click="dialogEditProduct = false">Batal</v-btn>
+              <v-btn color="primary" dark type="submit" :loading="loading">Tambahkan</v-btn>
+            </v-card-actions>
+          </v-form>
+        </ValidationObserver>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -107,6 +168,7 @@ const product = createNamespacedHelpers("product");
 export default {
   data() {
     return {
+      selectedProduct: {},
       search: null,
       nameProduct: null,
       priceProduct: null,
@@ -120,7 +182,8 @@ export default {
         { text: 'Deskripsi', value: 'description', sortable: false },
         { text: '', value: 'actions', sortable: false }
       ],
-      dialogAddProduct: false
+      dialogAddProduct: false,
+      dialogEditProduct: false
     }
   },
   computed: {
@@ -139,12 +202,24 @@ export default {
     valueCategory(item) {
       return item
     },
+    goToEdit(item) {
+      this.selectedProduct = item;
+      this.dialogEditProduct = true;
+    },
     goDelete(item) {
       this.deleteProduct(item)
         .then(result => {
           console.log(result);
           this.getProduct()
         })
+    },
+    editProduct() {
+      this.postProduct(this.selectedProduct)
+        .then(result => {
+          console.log(result);
+          this.dialogEditProduct = false;
+          this.getProduct()
+        });
     },
     addProduct() {
       let dataForm = {
