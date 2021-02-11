7c5c497ca87293c9536297f83261c837b7322c74
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2021-02-11 20:51:55 +0700

    :sparkles: feat(Dashboard): New layout

---
+++ src/views/Dashboard/components/LineChart.vue
@@ -0,0 +1,43 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: ['title'],
+  mounted() {
+    this.renderChart(
+      {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: 'Total Transaksi',
+            data: [2, 10, 5, 9, 0, 6, 20],
+            backgroundColor: "transparent",
+            borderColor: "#3987D5",
+            pointBackgroundColor: "#3987D5"
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: this.title
+        },
+        legend: {
+          display: false
+        }
+      }
+    );
+  }
+};
+</script>
--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -2,16 +2,94 @@
   <div>
     <div class="d-flex flex-row justify-space-between align-center">
       <h1>Dashboard</h1>
+      <div class="my-auto">
+        <v-menu
+          ref="filterDate"
+          v-model="filterDate"
+          :close-on-content-click="false"
+          :return-value.sync="selectedDate"
+          :nudge-right="-60"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="dateRangeText"
+              label="Pilih tanggal"
+              append-icon="mdi-calendar"
+              class="mt-3"
+              readonly
+              outlined
+              dense
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="selectedDate"
+            range
+          >
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              color="primary"
+              @click="filterDate = false"
+            >
+              Cancel
+            </v-btn>
+            <v-btn
+              text
+              color="primary"
+              @click="$refs.filterDate.save(selectedDate)"
+            >
+              OK
+            </v-btn>
+          </v-date-picker>
+        </v-menu>
+      </div>
+    </div>
+    <div class="mt-3">
+      <v-row>
+        <v-col cols="12" md="6" lg="6">
+          <v-card
+            outlined
+            flat
+            class="pa-4"
+          >
+            <line-chart :title="'Total Transaksi'"></line-chart>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6" lg="6">
+          <v-card
+            outlined
+            flat
+            class="pa-4"
+          >
+            <line-chart :title="'Total Pemasukan'"></line-chart>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
 
 <script>
+import lineChart from "./components/LineChart.vue";
+
 export default {
   components: {
+    lineChart
   },
   data() {
     return {
+      selectedDate: [],
+      filterDate: false
+    }
+  },
+  computed: {
+    dateRangeText () {
+      return this.selectedDate.join(' ~ ')
     }
   }
 }
