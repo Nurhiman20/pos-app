741cc19e68c5f4cd29b7ba599055f9cdd5b18c6b
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2021-02-20 09:41:18 +0700

    feat(Header): Input for select cashier and outlet

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,33 +13,28 @@
         <v-icon>mdi-menu</v-icon>
       </v-btn> -->
       <v-spacer></v-spacer>
-      <div>
-        <v-menu
-          bottom
-          offset-y
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn
-              v-on="on"
-              outlined
-              color="white"
-            >
-              Owner
-              <v-icon class="ml-1">mdi-chevron-down</v-icon>
-            </v-btn>
-          </template>
-          <v-card>
-            <v-list>
-              <v-list-item
-                v-for="(item, i) in items"
-                :key="i"
-                @click="() => {}"
-              >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-card>
-        </v-menu>
+      <div class="d-flex flex-row">
+        <v-select
+          v-model="cashier"
+          :items="$store.state.listCashier"
+          :item-text="textCashier"
+          :item-value="valueCashier"
+          label="Kasir"
+          class="mb-0 mt-7 ml-4 mr-2"
+          outlined
+          dense
+          @change="changeCashier"
+        ></v-select>
+        <v-select
+          v-model="outlet"
+          :items="$store.state.listOutlet"
+          :item-text="textOutlet"
+          :item-value="valueOutlet"
+          label="Outlet"
+          class="mb-0 mt-7"
+          outlined
+          dense
+        ></v-select>
       </div>
     </v-app-bar>
   </div>
@@ -51,6 +46,8 @@ export default {
   props: ['drawer'],
   data () {
     return {
+      outlet: null,
+      cashier: null,
       items: [
         {
           title: 'Profile'
@@ -60,6 +57,27 @@ export default {
         }
       ]
     }
+  },
+  methods: {    
+    textOutlet(item) {
+      return item.name
+    },
+    valueOutlet(item) {
+      return item
+    },
+    textCashier(item) {
+      return item.username
+    },
+    valueCashier(item) {
+      return item
+    },
+    changeCashier(e) {
+      localStorage.setItem('cashier', e.id);
+    }
+  },
+  created() {
+    this.$store.dispatch("getOutlet");
+    this.$store.dispatch("getCashier");
   }
 }
 </script>
