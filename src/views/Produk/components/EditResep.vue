cf0fdc8e598fcf5f75110e225e6bcd53972a59fd
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2021-02-24 06:01:55 +0700

    feat(Edit resep): Product name

--- src/views/Produk/components/EditResep.vue
+++ src/views/Produk/components/EditResep.vue
@@ -3,30 +3,16 @@
     <v-dialog v-model="show" persistent width="500">
       <v-card class="pa-3">
         <div class="d-flex flex-row justify-space-between align-center">
-          <v-card-title class="ml-0">Edit Resep</v-card-title>
+          <div class="d-flex flex-column">
+            <v-card-title class="ml-0">Edit Resep</v-card-title>
+            <v-card-subtitle>{{ selectedRecipe.product.name }}</v-card-subtitle>
+          </div>
           <v-btn color="error" outlined @click="goDelete">
             <v-icon class="mr-1">mdi-delete</v-icon>Hapus
           </v-btn>
         </div>
         <ValidationObserver ref="form" v-slot="{ handleSubmit }">
           <v-form @submit.prevent="handleSubmit(editRecipe)">
-            <ValidationProvider v-slot="{ errors }" name="Nama produk" rules="required">
-              <v-autocomplete
-                :error-messages="errors"
-                v-model="selectedRecipe.product"
-                :items="$store.state.listProduct"
-                :item-text="textProduct"
-                :item-value="valueProduct"
-                label="Nama Produk"
-                cache-items
-                class="mb-0 mt-2 px-4"
-                outlined
-                dense
-                hide-no-data
-                hide-details
-                :clearable="true"
-              ></v-autocomplete>
-            </ValidationProvider>
             <ValidationProvider v-slot="{ errors }" name="Varian" rules="required">
               <v-text-field
                 :error-messages="errors"
@@ -34,10 +20,10 @@
                 label="Varian"
                 outlined
                 dense
-                class="mb-0 mt-3 px-4"
+                class="mb-0 mt-6 px-4"
               ></v-text-field>
             </ValidationProvider>
-            <div class="px-4 mt-6">
+            <div class="px-4 mt-2">
               <v-data-table
                 :headers="headers"
                 :items="selectedRecipe.ingredients"
@@ -102,6 +88,7 @@ export default {
   },
   watch: {
     selected(val) {
+      console.log(val);
       this.selectedRecipe = val
     }
   },
