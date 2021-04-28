57d26480a6b0bdd0d9a77a609fd7da712d9aedeb
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2021-04-28 16:59:12 +0700

    feat(Transfer): Detail receive order dialog

---
+++ src/views/Inventaris/views/Transfer/components/DetailPesananDiterima.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <v-dialog v-model="show" persistent width="400">
+      <v-card class="pa-3">
+        <div class="d-flex flex-row justify-space-between align-center">
+          <v-card-title class="ml-0">Detail Pesanan Diterima</v-card-title>
+        </div>
+        <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+          <v-form @submit.prevent="handleSubmit(editOrder)">
+            <ValidationProvider v-slot="{ errors }" name="Cabang pengirim" rules="required">
+              <v-autocomplete
+                :error-messages="errors"
+                v-model="selectedOrder.destination_outlet"
+                :items="$store.state.listOutlet"
+                :item-text="textOutlet"
+                :item-value="valueOutlet"
+                label="Cabang Pengirim"
+                cache-items
+                class="mb-0 mt-2 px-4"
+                outlined
+                dense
+                hide-no-data
+                hide-details
+                disabled
+                :clearable="true"
+              ></v-autocomplete>
+            </ValidationProvider>
+            <div class="px-4 mt-2">
+              <v-data-table
+                :headers="headers"
+                :items="selectedOrder.ingredients"
+                class="elevation-1 scrollbar-custom mb-4"
+                hide-default-footer
+              ></v-data-table>
+            </div>
+            <!-- <div class="px-4 mt-3 mb-6">
+              <v-btn color="secondary" dark block @click="dialogAddIngredient = true">Tambah Bahan</v-btn>
+            </div> -->
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="warning darken-1" text @click="closeDialog">Tutup</v-btn>
+            </v-card-actions>
+          </v-form>
+        </ValidationObserver>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['show', 'selected'],
+  data() {
+    return {
+      selectedOrder: {},
+      selectedIngredient: {},
+      headers: [
+        { text: 'Bahan', value: 'ingredient.name', sortable: false },
+        { text: 'Qty', value: 'qty', sortable: false },
+        { text: 'Unit', value: 'ingredient.unit', sortable: false }
+      ]
+    }
+  },
+  watch: {
+    selected(val) {
+      this.selectedOrder = val
+    }
+  },
+  methods: {
+    textOutlet(item) {
+      return item.name
+    },
+    valueOutlet(item) {
+      return item
+    },
+    closeDialog() {
+      this.$emit('closeDialog', false);
+    }
+  }
+}
+</script>
--- src/views/Inventaris/views/Transfer/components/PesananDiterima.vue
+++ src/views/Inventaris/views/Transfer/components/PesananDiterima.vue
@@ -27,29 +27,43 @@
         :search="search"
         class="elevation-1 scrollbar-custom mt-3"
         hide-default-footer
+        @click:row="showDialogDetail"
       >
         <template v-slot:item.ingredients="{item}">
           <p class="my-auto">{{ item.ingredients.length }}</p>
         </template>
       </v-data-table>
     </v-card>
+
+    <receive-order-dialog
+      :show="dialogDetail"
+      :selected="selectedOrder"
+      @closeDialog="closeDialogDetail"
+    ></receive-order-dialog>
   </div>
 </template>
 
 <script>
+import receiveOrderDialog from './DetailPesananDiterima';
+
 export default {
+  components: {
+    receiveOrderDialog
+  },
   data() {
     return {
       search: null,
       select: null,
       itemOrder: [],
       listOrder: [],
+      selectedOrder: {},
       headers: [
         { text: 'ID Pesanan', value: 'id', sortable: false },
         { text: 'Waktu', value: 'time', sortable: true },
         { text: 'Cabang Pengirim', value: 'outlet.name', sortable: true },
         { text: 'Bahan', value: 'ingredients', sortable: false }
       ],
+      dialogDetail: false
     }
   },
   watch: {
@@ -64,6 +78,13 @@ export default {
         return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1;
       });
     },
+    closeDialogDetail(e) {
+      this.dialogDetail = e;
+    },
+    showDialogDetail(e) {
+      this.selectedOrder = e;
+      this.dialogDetail = true;
+    }
   },
 }
 </script>
